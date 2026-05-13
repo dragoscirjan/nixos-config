fa3789bc979c4f3e20f0639307034c6429b5bd7b
Dragos Cirjan <dragos@cirjan.pro>
2026-05-13 12:24:04 +0300

    chore: added gh extensions and hugo

--- modules/common/git.nix
+++ modules/common/git.nix
@@ -6,4 +6,13 @@
   forgejo-cli
   jujutsu
   lazygit
+
+  # Script to install gh extensions imperatively
+  (writeShellScriptBin "install-gh-extensions" ''
+    #!/usr/bin/env bash
+    echo "Installing gh extensions..."
+    ${gh}/bin/gh extension install dlvhdr/gh-dash || true
+    ${gh}/bin/gh extension install gennaro-tedesco/gh-i || true
+    echo "Extensions installed successfully!"
+  '')
 ]
